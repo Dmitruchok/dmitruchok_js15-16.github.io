83f22eac179868d10534cd05549834fe0d2ea539
Andrei Dmytruk <mallboroks@mail.ru>
2016-10-26 01:00:50 +0300

    15

--- js/script.js
+++ js/script.js
@@ -21,7 +21,7 @@ $form.click(function () {
 });
 
 $.ajax({
-  url: 'http://ajax.googleapis.com/ajax/services/search/web?v=1.0&key=ABQIAAAACKQaiZJrS0bhr9YARgDqUxQBCBLUIYB7IF2WaNrkYqF0tBovNBQFDtM_KNtb3xQxWff2mI5hipc3lg=large&q=name&callback=GoogleCallback&context=?',
+  url: 'http://ajax.googleapis.com/ajax/services/search/web?v=1.0&key=ABQIAAAACKQaiZJrS0bhr9YARgDqUxQBCBLUIYB7IF2WaNrkYqF0tBovNBQFDtM_KNtb3xQxWff2mI5hipc3lg=large&q=&callback=GoogleCallback&context=?',
 
   data: {
     abc: 123 //отправка на сервер
@@ -31,8 +31,7 @@ $.ajax({
 
   dataType: 'jsonp',
 
-  success: function () {
-
+  success: function (data) {
   },
 
   error: function () {
@@ -40,4 +39,42 @@ $.ajax({
   }
 });
 
+function Human() {
+  this.name = 'Andrei';
+  this.age = 25;
+  this.sex = 'male';
+  this.weight = 84;
+  this.height = 174;
+}
+
+function Worker() {
+  this.placeWork = 'Radischeva 10/14';
+  this.salary = '1300$';
+  this.work = function () {
+    console.log('Работать');
+  }
+}
+
+Worker.prototype = new Human();
+var newWorker = new Worker(),
+workerLast = new Worker(),
+workerSay = new Worker();
+console.log(newWorker.age);
+console.log(workerLast.sex);
+console.log(workerSay.work());
+
+function Student() {
+  this.placeStudy = 'M. Pechersky';
+  this.scholarship = '150$';
+  this.see = function () {
+    console.log('Смотреть фильмы');
+  }
+}
+
+Student.prototype = new Human();
+var newStudent = new Student(),
+studentLast = new Student();
+
+console.log(newStudent.height);
+console.log(studentLast.name);
 });
