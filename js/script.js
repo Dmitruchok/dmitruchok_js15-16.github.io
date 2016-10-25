6377f829b3e0c209a807264ad81dc578b30d48d0
Andrei Dmytruk <mallboroks@mail.ru>
2016-10-25 17:21:05 +0300

    16

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
 
+function GoogleCallback(jqueryObj, data) {
+console.log(data);
+}
+
 $(function() {
 
 var $form = $('.wrapper');
@@ -11,10 +15,30 @@ var $borderLight = false;
 
 });
 
-
 $form.click(function () {
  $findInform.removeClass('on_outline');
  $findInform.addClass('enter_text');
+});
+
+$.ajax({
+  url:
+  'http://ajax.googleapis.com/ajax/services/search/web?v=1.0?key=your-key&q=PHP&callback=GoogleCallback&context=?',
+
+  /*data: (
+    abc: 123 //отправка на сервер
+  ),*/
+
+  mathod: 'POST',//Отправить Get чтобы получить данные
+
+  dataType: 'jsonp',
+
+  success: function () {
+
+  },
+
+  error: function () {
+
+  }
 });
 
 });
